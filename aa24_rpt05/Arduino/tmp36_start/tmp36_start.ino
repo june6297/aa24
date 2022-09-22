55e8081443eda41d03115295d1a63c9f3bb38624
june6297 <62374111+june6297@users.noreply.github.com>
2022-09-22 17:19:07 +0900

    Add files via upload

---
+++ aa24_rpt05/Arduino/tmp36_start/tmp36_start.ino
@@ -0,0 +1,31 @@
+//
+// AA24, TMP36 sensor
+//
+
+#define TEMP_INPUT 0
+// or  int TEMP_INPUT = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+   //getting the voltage reading from the temperature sensor
+  int value = analogRead(TEMP_INPUT);
+  Serial.print("AA24, value =");
+  Serial.print(value);
+  Serial.print(" : ");
+  
+  // Print value
+  float voltage = value * 5.0 * 1000;
+  voltage /= 1023.0;
+
+  Serial.print(voltage);
+  Serial.print(" mV, ");
+
+  float temperatureC = (voltage -500) /10;
+  Serial.print(temperatureC);
+  Serial.println(" degrees C");
+
+  delay(1000);
+}
